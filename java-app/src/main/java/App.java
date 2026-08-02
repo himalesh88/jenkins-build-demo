65f02cc8713241e4115a400f999d5afd6095e739
himalesh <you@gmail.com>
2026-08-02 06:19:47 +0000

    jva class file update

--- java-app/src/main/java/App.java
+++ java-app/src/main/java/App.java
@@ -1,8 +1,3 @@
-package com.dev3l.hello_world.test;
-
-import org.junit.Assert;
-import org.junit.Test;
-
 public class App
 {
     public static void main(String[] args)
@@ -10,13 +5,5 @@ public class App
         System.out.println("Java ppplication started.");
         System.out.println("Build executed successfully from Jenkins.");
         System.out.println("Java application version: "+System.getProperty("java.version"));
-    }
-}
-
-class ExampleTest
-{
-    @Testpublic void exampleTest()
-    {
-        Assert.assertTrue(true);
     }
 }
