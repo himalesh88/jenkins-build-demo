c683c059dd8e9a673c3052c3890786723bfa0140
himalesh <you@gmail.com>
2026-08-02 05:54:02 +0000

    java file changed

--- python-app/app.py
+++ python-app/app.py
@@ -1,4 +1,7 @@
-
+print("Hello, World!")
+print("Welcome to Python Programming")
 print("Python application started.")
 print("Build executed successfully by Jenkins.")
 print(f"Python version: {platform.python_version()}")
+print("Learning Python is fun!")
+print("Have a great day!")
